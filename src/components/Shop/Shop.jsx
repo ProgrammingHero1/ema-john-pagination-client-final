2412341401ba04e70cfe08c1dfbbc8ecab6a3b5a
Programming Hero <programming.hero1@gmail.com>
2023-10-29 18:50:29 -0500

    dynamic items per page using dropdown

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,9 +7,10 @@ import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
     const { count } = useLoaderData();
-    const itemsPerPage = 10;
+    
     const numberOfPages = Math.ceil(count / itemsPerPage);
 
     // const pages = []
@@ -77,6 +78,13 @@ const Shop = () => {
         deleteShoppingCart();
     }
 
+    const handleItemsPerPage = e =>{
+        
+        const val = parseInt(e.target.value);
+        console.log(val);
+        setItemsPerPage(val)
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -102,6 +110,12 @@ const Shop = () => {
                 {
                     pages.map(page => <button key={page}>{page}</button>)
                 }
+                <select value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
